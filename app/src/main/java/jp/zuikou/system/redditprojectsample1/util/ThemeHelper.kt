670c46e69f7e57545b6d4b0d6734c130666504e3
QUAN NGUYEN VIET <quan@seesaa.co.jp>
2019-12-23 18:21:27 +0900

    implement dark mode

---
+++ app/src/main/java/jp/zuikou/system/redditprojectsample1/util/ThemeHelper.kt
@@ -0,0 +1,29 @@
+package jp.zuikou.system.redditprojectsample1.util
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatDelegate
+
+object ThemeHelper {
+
+    val LIGHT_MODE = "light"
+    val DARK_MODE = "dark"
+    val DEFAULT_MODE = "default"
+
+    fun applyTheme(themePref: String) {
+        when (themePref) {
+            LIGHT_MODE -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+            DARK_MODE -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            else -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
+                }
+            }
+        }
+    }
+}
